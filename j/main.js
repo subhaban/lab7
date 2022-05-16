c290c269e53a7a48f00e565cfcbffeb2c2ccdb31
Subha <subhaban20@gmail.com>
2022-05-16 08:58:41 -0700

    comit

--- j/main.js
+++ j/main.js
@@ -2,6 +2,17 @@ import {
     info
 } from './personal.js';
 
+//greetings for the page//
+
+const rememberDiv = document.querySelector('.remember');
+const forgetDiv = document.querySelector('.forget');
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#entername');
+const submitBtn = document.querySelector('#submitname');
+const forgetBtn = document.querySelector('#forgetname');
+
+const h1 = document.querySelector('h1');
+const personalGreeting = document.querySelector('.personal-greeting');
 
 //--Shortcut variables for the buttons and the body.
 //const container = document.querySelector('#container');
@@ -9,7 +20,40 @@ const btn = document.querySelector("#btn");
 const typeBtn = document.querySelector("#typebtn");
 const para = document.createElement('p');
 
-
+form.addEventListener('submit', e => e.preventDefault());
+submitBtn.addEventListener('click', () => {
+    // store the entered name in web storage
+    localStorage.setItem('name', nameInput.value);
+    // run nameDisplayCheck() to sort out displaying the personalized greetings and updating the form display
+    nameDisplayCheck();
+});
+// define the nameDisplayCheck() function
+function nameDisplayCheck() {
+    // check whether the 'name' data item is stored in web Storage
+    if(localStorage.getItem('name')) {
+      // If it is, display personalized greeting
+      const name = localStorage.getItem('name');
+      h1.textContent = `Welcome  ${name}`;
+      //personalGreeting.textContent = `Welcome to our website, ${name}! We hope you have fun while you are here.`;
+      // hide the 'remember' part of the form and show the 'forget' part
+      forgetDiv.style.display = 'block';
+      rememberDiv.style.display = 'none';
+    } else {
+      // if not, display generic greeting
+      h1.textContent = 'Welcome There';
+     // personalGreeting.textContent = 'Welcome to our website. We hope you have fun while you are here.';
+      // hide the 'forget' part of the form and show the 'remember' part
+      forgetDiv.style.display = 'none';
+      rememberDiv.style.display = 'block';
+    }
+  }
+  nameDisplayCheck();
+    forgetBtn.addEventListener('click', () => {
+        // Remove the stored name from web storage
+        localStorage.removeItem('name');
+        // run nameDisplayCheck() to sort out displaying the generic greeting again and updating the form display
+        nameDisplayCheck();
+    });
 // Adding Events to the butons----//
 // --- Event for Random Button ---//
 btn.addEventListener('click', (e) => {
@@ -22,11 +66,46 @@ typeBtn.addEventListener('click', (e) => {
     typeWord();
 })
 
+/// create database////
+let db;
+const openRequest = window.indexedDB.open('randomwordsdb', 1);
+// error handler signifies that the database didn't open successfully
+openRequest.addEventListener('error', () => console.error('Database failed to open'));
+
+// success handler signifies that the database opened successfully
+openRequest.addEventListener('success', () => {
+  console.log('Database opened successfully');
+
+  // Store the opened database object in the db variable. This is used a lot below
+  db = openRequest.result;
+
+  // Run the displayData() function to display the notes already in the IDB
+  displayData();
+});
+
+openRequest.addEventListener('upgradeneeded', e => {
+
+  // Grab a reference to the opened database
+  db = e.target.result;
+
+  // Create an objectStore to store our notes in (basically like a single table)
+  // including a auto-incrementing key
+  const objectStore = db.createObjectStore('notes_os', { keyPath: 'id', autoIncrement:true });
+
+  // Define what data items the objectStore will contain
+  objectStore.createIndex('title', 'title', { unique: false });
+  objectStore.createIndex('body', 'body', { unique: false });
+
+  console.log('Database setup complete');
+});
+
+
+
 //------Functions --------//
 //---Fuction for random words.--//
 //Using the fetch method from random word API--//
 const randomWord = () => {
-    fetch("https://random-word-api.herokuapp.com/word?number=1")
+    fetch("https://random-word-api.herokuapp.com/word?number=20")
         .then(response => {
             return response.json();
         })
